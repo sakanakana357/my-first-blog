aced78eb4537e0947c51d7a684008aa15dcb0b30
yuki <gmggr.11@gmail.com>
2022-04-08 23:20:50 +0900

    ok

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,10 @@ from django.utils import timezone
 class Post(models.Model): #models.Modelで、これがDjangoModelであり、データベースに置くものだと示す
 
     #各属性はすべてデータフィールドである
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
+from django.urls import path 
 from . import views
 
-#post_listというviewを""urlに割り当ててる
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    path("post/<pk>", views.post_detail, name="post_detail"),
-]             #↑引数
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path("post/<int:pk>/edit/", views.post_edit, name="post_edit")
+]
